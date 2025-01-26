2e58623c9137a1ad92d1355ccef0fd4d305946a3
Abdalrhman_Olimat <abdalrhmanolimat@gmail.com>
2025-01-26 20:30:52 +0300

    init

---
+++ includes/test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+# define TEST_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#endif 
---
+++ src/test.c
@@ -0,0 +1,6 @@
+#include "../includes/test.h"
+
+int main ()
+{
+
+}
